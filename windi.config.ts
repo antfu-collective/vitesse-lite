ab18fbd9aea006603026396b73e1360f1dbed1a1
Anthony Fu <anthonyfu117@hotmail.com>
2021-11-04 03:42:06 +0800

    chore: update

--- windi.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'windicss/helpers'
-
-export default defineConfig({
-  darkMode: 'class',
-  // https://windicss.org/posts/v30.html#attributify-mode
-  attributify: true,
-})
