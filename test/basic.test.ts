9c87f5b6fc0f8c1fe9c3ea276b05315cb24812e3
Anthony Fu <anthonyfu117@hotmail.com>
2021-12-26 13:14:48 +0800

    chore: test auto import

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,5 +1,3 @@
-import { describe, it, expect } from 'vitest'
-
 describe('Hi', () => {
   it('should works', () => {
     expect(1 + 1).toEqual(2)
--- test/component.test.ts
+++ test/component.test.ts
@@ -1,5 +1,4 @@
 import { mount } from '@vue/test-utils'
-import { describe, it, expect } from 'vitest'
 import Counter from '../src/components/Counter.vue'
 
 describe('Counter.vue', () => {
