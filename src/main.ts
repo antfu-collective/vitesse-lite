188b49ec7632e4f3d1e461b18bd486456905f3f5
Andrew Sayman <andrew.sayman@gmail.com>
2022-02-24 04:16:49 -0500

    fix: use the vite BASE_URL when creating the web history (#11)

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import 'uno.css'
 
 const app = createApp(App)
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
 app.use(router)
