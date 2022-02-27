6e4ea17ee4c1f397f0eb5a207ebb37a61a540134
Anthony Fu <anthonyfu117@hotmail.com>
2022-02-27 13:56:54 +0800

    chore: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
-import { presetAttributify, presetIcons, presetUno } from 'unocss'
 import Unocss from 'unocss/vite'
 
 export default defineConfig({
@@ -37,19 +36,8 @@ export default defineConfig({
     }),
 
     // https://github.com/antfu/unocss
-    Unocss({
-      shortcuts: [
-        ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
-        ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
-      ],
-      presets: [
-        presetUno(),
-        presetAttributify(),
-        presetIcons({
-          scale: 1.2,
-        }),
-      ],
-    }),
+    // see unocss.config.ts for config
+    Unocss(),
   ],
 
   // https://github.com/vitest-dev/vitest
