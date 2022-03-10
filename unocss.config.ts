b04737c802b1563feab145a5ce21caa127ae13b6
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-11 01:56:06 +0800

    chore: don't install webfont preset by default, close #12

--- unocss.config.ts
+++ unocss.config.ts
@@ -3,7 +3,6 @@ import {
   presetAttributify,
   presetIcons,
   presetUno,
-  presetWebFonts,
   transformerDirectives,
   transformerVariantGroup,
 } from 'unocss'
@@ -19,13 +18,6 @@ export default defineConfig({
     presetIcons({
       scale: 1.2,
       warn: true,
-    }),
-    presetWebFonts({
-      fonts: {
-        sans: 'DM Sans',
-        serif: 'DM Serif Display',
-        mono: 'DM Mono',
-      },
     }),
   ],
   transformers: [
