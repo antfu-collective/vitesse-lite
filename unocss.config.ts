2646c27ea62665f5f2183cccaf579af55b393dde
TTiip <2711430287@qq.com>
2022-05-10 15:41:11 +0800

    chore: 修改 `unocss.config.ts`

--- unocss.config.ts
+++ unocss.config.ts
@@ -3,31 +3,38 @@ import {
   presetAttributify,
   presetIcons,
   presetUno,
-  presetWebFonts,
+  presetWebFonts
   // transformerDirectives,
   // transformerVariantGroup,
 } from 'unocss'
 
 export default defineConfig({
+  // 自定义类名
+  rules: [
+    ['bg-box', { color: '#ff6700' }],
+    // 此处报错 d / 4 不用理会
+    [/^s-(\d+)$/, ([, d]) => ({ margin: `${d / 4}rem` })],
+    [/^p-(\d+)$/, match => ({ padding: `${match[1] / 4}rem` })]
+  ],
   shortcuts: [
     ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
-    ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
+    ['icon-btn', 'text-[0.9em] inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600']
   ],
   presets: [
     presetUno(),
     presetAttributify(),
     presetIcons({
       scale: 1.2,
-      warn: true,
+      warn: true
     }),
     presetWebFonts({
       fonts: {
         sans: 'DM Sans',
         serif: 'DM Serif Display',
-        mono: 'DM Mono',
-      },
-    }),
-  ],
+        mono: 'DM Mono'
+      }
+    })
+  ]
   // transformers: [
   //   transformerDirectives(),
   //   transformerVariantGroup(),
