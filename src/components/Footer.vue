0f87c0e2370c96dda520b648edcf75b5ebc7b366
Anthony Fu <anthonyfu117@hotmail.com>
2022-06-12 05:43:20 +0800

    fix: use passive icons for dark toggle, close #22

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,11 @@
 <template>
   <nav text-xl mt-6 inline-flex gap-2>
-    <button class="icon-btn !outline-none" @click="toggleDark()">
-      <div v-if="isDark" i-carbon-moon />
-      <div v-else i-carbon-sun />
+    <button icon-btn @click="toggleDark()">
+      <div dark:i-carbon-moon i-carbon-sun />
     </button>
 
     <a
-      class="icon-btn"
-      i-carbon-logo-github
+      icon-btn i-carbon-logo-github
       rel="noreferrer"
       href="https://github.com/antfu/vitesse-lite"
       target="_blank"
