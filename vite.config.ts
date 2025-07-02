336870f372d69022104b87f12e696ac27d974118
bee1an <137426505+bee1an@users.noreply.github.com>
2025-07-02 10:54:38 +0800

    fix: error occurred when using "definePage" (#55)

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,9 @@ export default defineConfig({
     },
   },
   plugins: [
+    // https://github.com/posva/unplugin-vue-router
+    VueRouter(),
+
     VueMacros({
       defineOptions: false,
       defineModels: false,
@@ -29,9 +32,6 @@ export default defineConfig({
         }),
       },
     }),
-
-    // https://github.com/posva/unplugin-vue-router
-    VueRouter(),
 
     // https://github.com/antfu/unplugin-auto-import
     AutoImport({
